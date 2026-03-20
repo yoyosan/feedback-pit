b63cdace53cf69e06760928dd5a70581bc97cbe6
Alex Garrett-Smith <alex@alex.gs>
2026-03-20 10:45:49 +0000

    Favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ config('app.name', 'Feedback') }}</title>
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @inertiaHead
 </head>
